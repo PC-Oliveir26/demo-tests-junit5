622e747f83f3fa71d4a618760f270995034f21a8
PC-Oliver26 <priscilacamileoliveira26@gmail.com>
2026-09-03 14:57:59 -0300

    productService

---
+++ src/main/java/product/ProductService.java
@@ -0,0 +1,28 @@
+package product;
+
+import java.sql.SQLException;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ProductService {
+
+	@Autowired 
+	private  ProductRepository productRepository;
+	private ValidationService validationService;
+	
+	public void save (Product product) {
+		if (this.validationService.canSave(product)) {
+			this.productRepository.save(null);
+		}
+	}
+	
+	public void remove(Long id) throws SQLException {
+		Optional<Product> productOptional = this.productRepository.findById(id);
+			if(productOptional.isPresent()) {
+				this.productRepository.deleteById(id);
+			} else {
+				throw new SQLException("product not registed or not found");
+			}
+	}
+}
