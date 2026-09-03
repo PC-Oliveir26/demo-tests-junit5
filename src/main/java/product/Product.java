3c77ad9ad0c42887dc54722997639ff4c5f02e0e
PC-Oliver26 <priscilacamileoliveira26@gmail.com>
2026-09-03 14:55:44 -0300

    product

---
+++ src/main/java/product/Product.java
@@ -0,0 +1,39 @@
+package product;
+
+import jakarta.persistence.Id;
+
+public class Product {
+
+	@Id
+	private Long id;
+	private String name;
+	private Double value;
+	
+	public Product() {
+		super();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Double getValue() {
+		return value;
+	}
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	public Object getStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
