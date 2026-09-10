b9bc6737be47d4a9031770480c79f29098a4fcc6
PC-Oliveir26 <priscilacamileoliveira26@gmail.com>
2026-09-10 15:01:26 -0300

    basic operations test

--- tests-with-junit-java/src/test/java/math/SimpleMathTest.java
+++ tests-with-junit-java/src/test/java/math/SimpleMathTest.java
@@ -1,15 +1,36 @@
 package math;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class SimpleMathTest {
 
 	@Test
-	void test() {
+	void testSum() {
 		SimpleMath math = new SimpleMath();
+		Double actual = math.sum(35.0, 10.0);
+		Assertions.assertEquals(45, actual);
+//		System.out.print(actual);
+	}
 	
+	@Test 
+	void testSubtraction() {
+		SimpleMath math = new SimpleMath();
+		Double actual = math.subtraction(35.0, 10.0);
+		Assertions.assertEquals(25, actual);
+	}
 	
+	@Test
+	void testDivision() {
+		SimpleMath math = new SimpleMath();
+		Double actual = math.division(10.0, 2.0);
+		Assertions.assertEquals(5, actual);
 		
 	}
+	@Test
+	void testMulplication() {
+		SimpleMath math = new SimpleMath();
+		Double actual = math.multiplication(5.0, 2.0);
+		Assertions.assertEquals(10, actual);}
 
 }
