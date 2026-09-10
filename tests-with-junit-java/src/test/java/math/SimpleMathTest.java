4a6080b5c6980a47d125d5b1d6d0d89a55fce50c
PC-Oliveir26 <priscilacamileoliveira26@gmail.com>
2026-09-10 15:03:34 -0300

    basic operations test

--- tests-with-junit-java/src/test/java/math/SimpleMathTest.java
+++ tests-with-junit-java/src/test/java/math/SimpleMathTest.java
@@ -8,7 +8,7 @@ class SimpleMathTest {
 	@Test
 	void testSum() {
 		SimpleMath math = new SimpleMath();
-		Double actual = math.sum(35.0, 10.0);
+		Double actual = math.sum(35D, 10D);
 		Assertions.assertEquals(45, actual);
 //		System.out.print(actual);
 	}
@@ -16,21 +16,21 @@ class SimpleMathTest {
 	@Test 
 	void testSubtraction() {
 		SimpleMath math = new SimpleMath();
-		Double actual = math.subtraction(35.0, 10.0);
+		Double actual = math.subtraction(35D, 10D);
 		Assertions.assertEquals(25, actual);
 	}
 	
 	@Test
 	void testDivision() {
 		SimpleMath math = new SimpleMath();
-		Double actual = math.division(10.0, 2.0);
+		Double actual = math.division(10D, 2D);
 		Assertions.assertEquals(5, actual);
 		
 	}
 	@Test
 	void testMulplication() {
 		SimpleMath math = new SimpleMath();
-		Double actual = math.multiplication(5.0, 2.0);
+		Double actual = math.multiplication(5D, 2D);
 		Assertions.assertEquals(10, actual);}
 
 }
