48f8298609f6aaf4cb13c5da26b2683bfc8442ec
PC-Oliveir26 <priscilacamileoliveira26@gmail.com>
2026-09-10 15:29:50 -0300

    assert message and lambda function

--- tests-with-junit-java/src/test/java/math/SimpleMathTest.java
+++ tests-with-junit-java/src/test/java/math/SimpleMathTest.java
@@ -10,31 +10,44 @@ class SimpleMathTest {
 	@Test
 	void testSum() {
 		SimpleMath math = new SimpleMath();
-		Double actual = math.sum(35D, 10D);
-		
-		assertEquals(45, actual);
+		Double firstNumber = 35D;
+		Double secondNumber = 10D;
+		Double actual = math.sum(firstNumber, secondNumber);
+		Double expected = 45D;
+
+		assertEquals(expected, actual, () -> 
+						firstNumber + 
+						" + " +
+						secondNumber +
+						"did not produce " +
+						expected +
+						" !"
+						);
 		assertNotEquals(5.5, actual);
 		assertNotNull(actual);
+
 	}
-	
-	@Test 
+
+	@Test
 	void testSubtraction() {
 		SimpleMath math = new SimpleMath();
 		Double actual = math.subtraction(35D, 10D);
 		Assertions.assertEquals(25, actual);
 	}
-	
+
 	@Test
 	void testDivision() {
 		SimpleMath math = new SimpleMath();
 		Double actual = math.division(10D, 2D);
 		Assertions.assertEquals(5, actual);
-		
+
 	}
+
 	@Test
 	void testMulplication() {
 		SimpleMath math = new SimpleMath();
 		Double actual = math.multiplication(5D, 2D);
-		Assertions.assertEquals(10, actual);}
+		Assertions.assertEquals(10, actual);
+	}
 
 }
