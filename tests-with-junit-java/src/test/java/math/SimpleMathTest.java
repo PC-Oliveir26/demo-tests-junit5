f3921717dbac908efddb90a89e849e312d69124b
PC-Oliveir26 <priscilacamileoliveira26@gmail.com>
2026-09-11 14:52:17 -0300

    default for nomenclature

--- tests-with-junit-java/src/test/java/math/SimpleMathTest.java
+++ tests-with-junit-java/src/test/java/math/SimpleMathTest.java
@@ -11,25 +11,25 @@ class SimpleMathTest {
 	String message = "didn't expected";
 
 	@Test
-	void testSum() {
+	void testSum_When_ThintyFiveIsAddedByTen_ShouldReturnFortyFive() {
 		Double actual = math.sum(35D, 10D);
 		assertEquals(45, actual, () -> message);
 	}
 
 	@Test
-	void testSubtraction() {
+	void testSubtraction_When_ThintyFiveSubtractedByTen_ShouldReturnTwentyFive() {
 		Double actual = math.subtraction(35D, 10D);
 		Assertions.assertEquals(25, actual, () -> message);
 	}
 
 	@Test
-	void testDivision() {
+	void testDivision_When_TenDividedByTwo_ShouldReturnFive() {
 		Double actual = math.division(10D, 2D);
 		Assertions.assertEquals(5, actual, () -> message);
 	}
 
 	@Test
-	void testMulplication() {
+	void testMulplication_When_FiveMultipliedByTwo_ShouldReturnTen() {
 		Double actual = math.multiplication(5D, 2D);
 		Assertions.assertEquals(10, actual, () -> message);
 	}
@@ -45,4 +45,7 @@ class SimpleMathTest {
 		Double actual = math.squareRoot(144D);
 		Assertions.assertEquals(12, actual);
 	}
+	
+//	Default:
+//	test[System Under Test]_[Condition or Stage Change]_[Expected Resume]
 }
