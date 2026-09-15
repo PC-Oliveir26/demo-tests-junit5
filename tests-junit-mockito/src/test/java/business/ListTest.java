0d184dab515965cfaedb5e2460b19c584c6789d4
PC-Oliveir26 <priscilacamileoliveira26@gmail.com>
2026-09-15 15:28:47 -0300

    mocking exception

--- tests-junit-mockito/src/test/java/business/ListTest.java
+++ tests-junit-mockito/src/test/java/business/ListTest.java
@@ -3,6 +3,8 @@ package business;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -24,30 +26,40 @@ public class ListTest {
 	void testMockingList_When_aizeIsCalled_ShouldReturn10() {
 		when(list.size()).thenReturn(10).thenReturn(15).thenReturn(55); // thenReturn() -> "return a specific,
 																		// predefined value
-
 		assertEquals(10, list.size());
 		assertEquals(15, list.size());
 		assertEquals(55, list.size());
-
 	}
+
 	@Test
 	void testMockingList_When_GetIsCalled_ShoudReturnName() {
 		var list = mock(List.class);
 		when(list.get(0)).thenReturn("Pc_Oliver");
-		
+
 		assertEquals("Pc_Oliver", list.get(0));
 		assertNull(list.get(1));
-		
-		
 	}
+
 	@Test
 	void testMockingList_When_GetIsCalledWithArgumentMatcher_ShoudReturnName() {
 		var list = mock(List.class);
 		when(list.get(anyInt())).thenReturn("Pc_Oliver");
-		
+
 		assertEquals("Pc_Oliver", list.get(anyInt()));
 		assertNotNull(list.get(anyInt()));
 //		assertNull(list.get(anyInt()));
+	}
+
+//	throws 
+	@Test
+	void testMockingList_When_ThrowsAnException() {
+		var list = mock(List.class);
 		
+		when(list.get(anyInt())).thenThrow(new RuntimeException("Foo Bar!!"));
+
+		assertThrows(RuntimeException.class, 
+				() -> { list.get(anyInt()); },
+				() -> "Should have throw an RuntimeException");
+	
 	}
 }
